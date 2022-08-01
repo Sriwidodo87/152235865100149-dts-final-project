e7f1db212db7e2f9f3a50981a1d8dd9107df5760
WithDodo <105720297+Sriwidodo87@users.noreply.github.com>
2022-08-01 21:11:05 +0800

    hapus

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import { Grid } from "@mui/material";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-import Modal from "../component/BasicModal";
+
 const Product=()=>{
 
     return(
     <div>
-        <Modal ></Modal>
+       <h4>Ini untuk Form Add product</h4>
     </div>
     )
 
